e1bda6971b5e4b3b19655c8f915860107cbdc061
Peter Saxton <peterhsaxton@gmail.com>
2020-03-05 10:45:35 +0000

    Add DID authentication

--- examples/myapp/app.js
+++ examples/myapp/app.js
@@ -2,10 +2,12 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var cookieSession = require("cookie-session");
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var sessionRouter = require("./routes/session");
 
 var app = express();
 
@@ -17,10 +19,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+var { SESSION_SECRET } = process.env;
+app.use(cookieSession({ name: "myapp", secret: SESSION_SECRET }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/session", sessionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
